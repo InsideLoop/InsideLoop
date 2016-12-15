f8d9b483e5ccd2db0574d3e8ea13985b52d93497
Francois Fayard <fayard@insideloop.io>
2016-12-15 18:08:35 +0100

    Added a Sobol random number generator

---
+++ il/random/sobol.h
@@ -0,0 +1,37 @@
+//==============================================================================
+//
+//                                  InsideLoop
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.txt for details.
+//
+//==============================================================================
+
+#ifndef IL_SOBOL_H
+#define IL_SOBOL_H
+
+#include <il/Array2C.h>
+
+#ifdef IL_MKL
+#include <mkl.h>
+#endif
+
+namespace il {
+
+inline il::Array2C<double> sobol(il::int_t nb_point, il::int_t dim, double a,
+                                 double b) {
+  il::Array2C<double> A{nb_point, dim};
+
+  VSLStreamStatePtr stream;
+  const MKL_INT brng = VSL_BRNG_SOBOL;
+  int error_code;
+  error_code = vslNewStream(&stream, brng, static_cast<MKL_INT>(dim));
+  error_code = vdRngUniform(VSL_RNG_METHOD_UNIFORM_STD, stream, dim * nb_point,
+                            A.data(), a, b);
+  error_code = vslDeleteStream(&stream);
+
+  return A;
+}
+}
+
+#endif  // IL_SOBOL_H
--- main.cpp
+++ main.cpp
@@ -7,74 +7,33 @@
 //
 //==============================================================================
 
+#include <cmath>
 #include <cstdio>
 
-#include <il/Timer.h>
-#include <il/linear_algebra/factorization/Cholesky.h>
-#include <il/linear_algebra/factorization/HouseHolder.h>
-#include <il/linear_algebra/factorization/PartialLU.h>
-#include <il/linear_algebra/factorization/Singular.h>
-#include <il/linear_algebra/factorization/Eigen.h>
+#include <il/Array3D.h>
+#include <il/io/png.h>
+#include <il/random/sobol.h>
 
 int main() {
-  const il::int_t n = 10000;
-  il::Array2D<double> A{n, n};
-  for (il::int_t j = 0; j < n; ++j) {
-    for (il::int_t i = 0; i < n; ++i) {
-      A(i, j) = 1.0 / (1 + il::abs(i - j));
-    }
+  const il::int_t n = 500;
+  const il::int_t nb_point = 100000;
+  const il::int_t dim = 2;
+
+  il::Array2C<double> A = il::sobol(nb_point, dim, 0.0, 1.0);
+
+  il::Array3D<unsigned char> image{n, n, 3, 255};
+  for (il::int_t i = 0; i < nb_point; ++i) {
+    il::int_t kx = std::floor(n * A(i, 0));
+    il::int_t ky = std::floor(n * A(i, 1));
+    image(kx, ky, 0) = 0;
+    image(kx, ky, 1) = 0;
+    image(kx, ky, 2) = 0;
   }
 
-  il::LowerArray2D<double> B{n};
-  for (il::int_t j = 0; j < n; ++j) {
-    for (il::int_t i = j; i < n; ++i) {
-      B(i, j) = 1.0 / (1 + il::abs(i - j));
-    }
-  }
-
-  il::Timer timer_llt{};
   il::Status status{};
-  il::Cholesky<il::Array2D<double>> llt_decomposition{A, il::io, status};
-  status.abort_on_error();
-  timer_llt.stop();
-
-  il::Timer timer_llt_lower{};
-  il::Cholesky<il::LowerArray2D<double>> llt_lower_decomposition{B, il::io,
-                                                                 status};
-  status.abort_on_error();
-  timer_llt_lower.stop();
-
-  il::Timer timer_lu{};
-  il::PartialLU<il::Array2D<double>> lu_decomposition{A, il::io, status};
-  status.abort_on_error();
-  timer_lu.stop();
-
-  il::Timer timer_house_holder{};
-  il::HouseHolder<il::Array2D<double>> house_holder_decomposition{A};
-  timer_house_holder.stop();
-
-  il::Timer timer_eigen{};
-  il::Eigen<il::Array2D<double>> eigen_decomposition{ A, il::io, status};
-  status.abort_on_error();
-  timer_eigen.stop();
-
-  il::Timer timer_svd{};
-  il::Singular<il::Array2D<double>> svd_decomposition{ A, il::io, status};
-  status.abort_on_error();
-  timer_svd.stop();
-
-  std::printf("                    Time for Cholesky decomposition: %7.2f s\n",
-              timer_llt.time());
-  std::printf("           Time for Cholesky (packed) decomposition: %7.2f s\n",
-              timer_llt_lower.time());
-  std::printf("                          Time for LU decomposition: %7.2f s\n",
-              timer_lu.time());
-  std::printf("                 Time for HouseHolder decomposition: %7.2f s\n",
-              timer_house_holder.time());
-  std::printf("   Time for Eigen Value Decomposition decomposition: %7.2f s\n",
-              timer_eigen.time());
-  std::printf("Time for Singular Value Decomposition decomposition: %7.2f s\n",
-              timer_svd.time());
+  il::save(image, std::string{"/home/fayard/Desktop/sobol.png"}, il::png,
+           il::io, status);
+  status.ignore_error();
 
   return 0;
 }
